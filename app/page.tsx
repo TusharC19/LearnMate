35d662f76f0987fb219dbde3398168fa126f2c96
TusharC19 <tusharchaturvedi199unova@gmail.com>
2025-11-27 23:00:14 +0530

    HomePage part-1 Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,49 @@
 import React from 'react'
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
-    <div>
-    <h1 className="text-2xl">Welcome</h1>
-    <Button></Button>
+    <main>
+    <h1 className="text-2xl">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+        id="456"
+        name="jfj"
+        topic="NLP"
+        subject="English"
+        duration={45}
+        color="#ffda6e"
+        />
+        <CompanionCard
+        id="457"
+        name="abc"
+        topic="Machine Learning"
+        subject="Math"
+        duration={30}
+        color="#a1cfff"
+        />
+        <CompanionCard
+        id="458"
+        name="xyz"
+        topic="Data Science"
+        subject="Science"
+        duration={60}
+        color="#ff9a9a"
+        />
+
+      </section>
+
+      <section className="home-section">
+        <CompanionsList/>
+        <CTA/>
+      </section>
+   
   
 
 
-    </div>
+    </main>
   )
 }
 
