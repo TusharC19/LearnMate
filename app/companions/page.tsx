7e9e4e0b890cee41aa9b719414550606fa0b8fd1
TusharC19 <tusharchaturvedi199unova@gmail.com>
2025-11-28 20:51:00 +0530

    Implemented SearchInput and SubjectFilter components along with server action for each item

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import {getAllCompanions} from "@/lib/actions/companion.action";
+import CompanionCard from "@/components/CompanionCard";
+import {getSubjectColor} from "@/lib/utils";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 
-const Page = () => {
+// to use search params in server components
+const ComapanionsLibaray = async ({searchParams}:SearchParams)  =>{
+    const filters = await searchParams;
+    const subject = filters.subject ? filters.subject : ' ';
+    const topic = filters.topic ? filters.topic : ' ';
+    const companions = await getAllCompanions({subject, topic});
+    console.log(companions);
+
+    // console.log('PARAMS',params);
+
+
     return (
-        <div>ComponentName</div>
+        <main>
+            <section className="flex justify-between gap-4 max-sm:flex-col">
+                <h1>Companion Library</h1>
+                <div className="flex gap-4">
+                    <SearchInput/>
+                    <SubjectFilter/>
+                </div>
+            </section>
+            <section className="companions-grid">
+                {companions.map((companion) => (
+                    <CompanionCard key={companion.id} {...companion}
+                    color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
+        </main>
     )
 }
 
-export default Page
+export default ComapanionsLibaray
